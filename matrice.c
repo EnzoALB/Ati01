e83b507970f8b403cb5b9330b3a2d8cfcf4514ff
EnzoALB <enzo.albouy0@gmail.com>
2020-02-24 15:21:35 +0100

    Ajout de la fonction addition et multiplication

--- matrice.c
+++ matrice.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 
-int** maketab(int l, int c){
+int** makematrix(int l, int c){
 	int** tablo ;
 	int i ;
 	tablo=malloc(l*sizeof(int*));
@@ -13,47 +13,131 @@ int** maketab(int l, int c){
 	return tablo ;
 }
 
-void remptab(int **tab, int ligne, int colonne){
+void freematrix(int **matrix, int ligne){
+	for(int i=0;i<ligne;i++){
+		free(matrix[i]);
+	}
+	free(matrix);
+}
+
+void rempmatrix(int **matrix, int ligne, int colonne){
 	int l;
 	int c;
 	for(l=0;l<ligne;l++){
 		for(c=0;c<colonne;c++){
-			tab[l][c]=rand()%99;
+			matrix[l][c]=rand()%99;
 		}
 	}
 }
 
-void printtab(int **tab, int ligne, int colonne){
+void printmatrix(int **matrix, int ligne, int colonne){
 	int l;
 	int c;
 	for(l=0;l<ligne;l++){
 		for(c=0;c<colonne;c++){
-			printf("%d ",tab[l][c]);
+			printf("%d ",matrix[l][c]);
 		}
 		printf("\n");
 	}
 }
+
+void addmatrix(int **matrix, int ligne, int colonne, int **matrix2, int ligne2, int colonne2, int **matrixadd){
+	if(ligne==ligne2){
+		if(colonne==colonne2){
+			int l;
+			int c;
+			for(l=0;l<ligne;l++){
+                		for(c=0;c<colonne;c++){
+               				matrixadd[l][c]=matrix[l][c]+matrix2[l][c];
+				}
+			}
+		}
+		else{
+			printf("Les colonnes ne sont pas égales\n");
+		}
+	}
+	else{
+		printf("Les lignes ne sont pas égales\n");
+        }
+}
+
+void mulmatrix(int **matrix, int ligne, int colonne, int **matrix2, int ligne2, int colonne2, int **matrixmul){
+	if(colonne==ligne2){
+		int l;
+		int c;
+		int x;
+		int somme;
+		somme=0;
+
+		for(l=0;l<ligne;l++){
+                	for(c=0;c<colonne2;c++){
+				somme=0;
+               			for(x=0;x<colonne;x++){
+					//printf("On va multiplier %d et ",matrix[l][x]);
+					//printf("%d\n",matrix2[x][c]);		
+					somme=matrix[l][x]*matrix2[x][c]+somme;
+				}
+				matrixmul[l][c]=somme;
+			}
+		}
+	}
+	else{
+		printf("Les matrices ne sont pas multipliables\n");
+	}
+}
+
 		
 
 int main (){
-time_t t;
-srand((unsigned) time(&t));
+srand((unsigned) time(NULL));
+
 int li;
 int col;
+int li2;
+int col2;
+int limul;
+int colmul;
+printf("Entrez le nombre de lignes de la première matrice \n");
+scanf("%d",&li);
+printf("Entrez le nombre de colonnes de la première matrice \n");
+scanf("%d",&col);
+printf("Entrez le nombre de lignes de la seconde matrice \n");
+scanf("%d",&li2);
+printf("Entrez le nombre de colonnes de la seconde matrice \n");
+scanf("%d",&col2);
 
-li=4;
-col=3;
+
 printf("\n");
-int** tab;
-tab = maketab(li,col);
-remptab(tab,li,col);
-printtab(tab,li,col);
+int** matrix;
+matrix = makematrix(li,col);
+rempmatrix(matrix,li,col);
+printmatrix(matrix,li,col);
 printf("\n");
-int** tab2;
-tab2 = maketab(li,col);
-remptab(tab2,li,col);
-printtab(tab2,li,col);
+
+int** matrix2;
+matrix2 = makematrix(li2,col2);
+rempmatrix(matrix2,li2,col2);
+printmatrix(matrix2,li2,col2);
 printf("\n");
+
+int** matrixadd;
+matrixadd = makematrix(li,col);
+addmatrix(matrix,li,col,matrix2,li2,col2,matrixadd);
+printmatrix(matrixadd,li,col);
+printf("\n");
+
+limul=li;
+colmul=col2;
+int** matrixmul;
+matrixmul = makematrix(limul,colmul);
+mulmatrix(matrix,li,col,matrix2,li2,col2,matrixmul);
+printmatrix(matrixmul,limul,colmul);
+printf("\n");
+
+freematrix(matrixmul,limul);
+freematrix(matrixadd,li);
+freematrix(matrix2,li2);
+freematrix(matrix,li);
 }
 
 	
