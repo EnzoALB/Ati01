64a071793aae1df8222ed8d180ac09625599610b
EnzoALB <enzo.Albouy0@gmail.com>
2020-03-02 09:14:51 +0100

    Création de la factorielle en itératif et début de la fonction déterminant

---
+++ factIte.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void factoriel(int fac){
+	int res;
+	int x;
+	res=fac;
+	for(x=1;x<fac;x++){
+		res=res*x;
+		//printf("x=%d  res=%d\n",x,res);
+	}
+	printf("%d! = %d",fac,res);
+}
+
+
+int main (){
+	int fac;
+	printf("Entrez le chiffre en factorielle : \n");
+	scanf("%d",&fac);
+	printf("\n");
+
+	factoriel(fac);
+	printf("\n");
+}
